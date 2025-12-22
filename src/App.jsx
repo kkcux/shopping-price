5d1f9fc13095247973dd4c092d44f93de3bdc3f5
KitsadaBuree <summer.buree@gmail.com>
2025-12-22 18:07:09 +0700

    update app.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom'; // 1. เพิ่ม Navigate ตรงนี้
 import Login from './components/Login/Login';
@@ -14,19 +13,6 @@ function App() {
         <Route path="/register" element={<Register />} />
       </Routes>
     </div>
-=======
-import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/register";
-import "./App.css";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
->>>>>>> eecc526f8cd5d825c595c4fc4e4f39dc4588bf9d
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' // 1. เพิ่มบรรทัดนี้
 import './index.css'
 import App from './App.jsx'
-=======
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App.jsx";
->>>>>>> eecc526f8cd5d825c595c4fc4e4f39dc4588bf9d
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-<<<<<<< HEAD
   </StrictMode>,
 )
-=======
-  </StrictMode>
-);
->>>>>>> eecc526f8cd5d825c595c4fc4e4f39dc4588bf9d
