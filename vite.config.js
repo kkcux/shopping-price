5f0cd1eec45fc01b6ae2f4e7980a654fae162130
kkcux <kitsada.buree@gmail.com>
2026-01-16 14:44:27 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // ส่วนที่เพิ่มใหม่เพื่อแก้ Warning เรื่องขนาดไฟล์ 👇
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   server: {
     headers: {
       // เปลี่ยนเป็น unsafe-none เพื่อปลดล็อกทุกอย่าง
